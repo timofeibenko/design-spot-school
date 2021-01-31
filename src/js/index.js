bad3c54c89164d51c2fef3f61ef691f80eaf02c5
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-01-31 21:50:55 +0300

    add webpack

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import '/index.html'
+
+import '../style/main.scss'
+
+import '../img/header/thumbnail.png'
+import '../img/main/aquamarine-polka-dot.svg'
+import '../img/main/blue-polka-dot.svg'
+import '../img/main/blue-stripes.svg'
+import '../img/main/nikita-zenchenko-photo.png'
+import '../img/main/telegram-icon.svg'
+import '../img/main/thumbnail.png'
+import '../img/aquamarine-stripes.svg'
+
+
+console.log('i compiled')
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/js/index.js'
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+    resolve: {
+        alias: {
+            styles: path.join(__dirname, 'src', 'style')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+        ],
+    },
+}
