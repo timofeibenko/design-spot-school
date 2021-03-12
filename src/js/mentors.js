85cc35afc264598d64382ea51ecc9d40117b2569
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-13 00:08:14 +0300

    refactor: make a separate module for mentors functionality

---
+++ src/js/mentors.js
@@ -0,0 +1,53 @@
+import Swiper from 'swiper/bundle';
+import '../../node_modules/swiper/swiper-bundle.min.css';
+
+import NikZen from  './../img/main/nikita-zenchenko-photo.png';
+import placeholder from './../img/main/mentors-placeholder.jpg'
+
+// Vars
+
+const mentorsPrevBtn = document.querySelector('.mentors__carousel-prev');
+const mentorsNextBtn = document.querySelector('.mentors__carousel-next');
+
+const mentorsSlides = document.querySelectorAll('.mentors__commentary-container');
+const mentorsCommsQty = document.querySelector('#mentorsCommsQty');
+
+// Photos
+
+const photos = document.querySelectorAll('.mentors__photo');
+
+photos.forEach((photo, index) => {
+    if (index === 0) photo.src = NikZen
+    else photo.src = placeholder
+});
+
+// Commentary count
+
+mentorsCommsQty.innerHTML = mentorsSlides.length.toString();
+
+// Commentary slider (Swiper)
+
+const mentorsSwiper = new Swiper('.mentors__swiper-container', {
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 10,
+});
+
+mentorsPrevBtn.addEventListener('click', () => mentorsSwiper.slidePrev());
+mentorsNextBtn.addEventListener('click', () => mentorsSwiper.slideNext());
+
+mentorsSwiper.on("slideChange", function() {
+    if (this.realIndex === 0) {
+        mentorsPrevBtn.classList.add('inactive')
+    }
+
+    else if (this.realIndex + 1 === mentorsSlides.length) {
+        mentorsNextBtn.classList.add('inactive')
+    }
+
+    else {
+        mentorsPrevBtn.classList.remove('inactive');
+        mentorsNextBtn.classList.remove('inactive');
+    }
+});
