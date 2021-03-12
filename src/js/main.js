eeb5e270fb4ef463da42d958a5968a20cb251a69
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-12 23:38:57 +0300

    feat: add placeholder for mentors slides 2-...

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
-import '../style/main.scss'
-import '../../node_modules/swiper/swiper-bundle.min.css'
+import '../style/main.scss';
+import '../../node_modules/swiper/swiper-bundle.min.css';
+
+import NikZen from  './../img/main/nikita-zenchenko-photo.png';
+import placeholder from './../img/main/mentors-placeholder.jpg'
 
 import Swiper from 'swiper/bundle';
 
-// Mentors | slider & comms qty
+// Mentors | slider, comms qty & photo
+
+const photos = document.querySelectorAll('.mentors__photo');
+
+photos.forEach((photo, index) => {
+    if (index === 0) photo.src = NikZen
+    else photo.src = placeholder
+});
 
 const mentorsSwiper = new Swiper('.mentors__swiper-container', {
     direction: 'horizontal',
