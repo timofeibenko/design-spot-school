2ae61f4dea45f9db0fd061e7958665705f6a2b36
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-15 01:46:48 +0300

    chore: delete commented previous code

--- src/js/qna.js
+++ src/js/qna.js
@@ -1,15 +1,7 @@
-// const listItem = document.querySelector('#qnaListItem');
-// const answer = document.querySelector('#qnaAnswer')
-// const toggler = document.querySelector('#qnaToggle');
-//
-// listItem.addEventListener('click', () => {
-//     listItem.classList.toggle('active')
-// })
-
 const qnaListItems = document.querySelectorAll('.qna__list-item');
 
 qnaListItems.forEach((listItem) => {
     listItem.addEventListener('click', (ev) => {
         listItem.classList.toggle('active')
     })
-})
+});
