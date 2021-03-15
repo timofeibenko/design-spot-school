78e5b9ededf743c586e54f3ffbf9ac02dddaf428
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-15 16:26:41 +0300

    import burger module

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import '../style/main.scss';
-import '../js/declinationOfNum'
+import '../js/declinationOfNum';
+import '../js/burger';
 import '../js/mentors';
 import '../js/reviews';
 import '../js/qna';
